c7749eac8640054c1474b984ea4553460d5fcf41
joshrint <josh.rint@gmail.com>
2022-05-27 11:53:28 -0700

    testing CI

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('url is correct', () =>{
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain('reactjs.org');
+})
