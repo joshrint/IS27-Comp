b9c717b12d758b898d4c253b7eb66cb01125f252
joshrint <josh.rint@gmail.com>
2022-05-28 13:20:46 -0700

    Build fail check

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,6 @@
 import './App.css';
-import { useFetch } from './utils/Hooks';
 
 function Home() {
-    //const [data] = useFetch('https://is27-comp-backend.azurewebsites.net/boatAPI');
   return (
     <div>
       <h1>home Page</h1>
