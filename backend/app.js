24b0745d2d437e3890085de3fec7d41f84afd2ed
joshrint <josh.rint@gmail.com>
2022-05-28 08:21:52 -0700

    Testing Push to Azure

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boatToursRouter = require('./routes/boatTours');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/boatToursApi", boatToursRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
